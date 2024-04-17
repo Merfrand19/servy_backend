ca32983cf74324364bac2f1f87ae54b41bf2f4d6
Merfrand LOKONON <gratiaslokonon19@gmail.com>
2024-04-17 11:42:48 +0200

    commit initial

--- app.js
+++ app.js
@@ -8,22 +8,13 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
-
 // Import the functions you need from the SDKs you need
 const firebase = require('firebase');
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
-};
-
+const firebaseConfig = require('./config/firebaseConfig');
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
